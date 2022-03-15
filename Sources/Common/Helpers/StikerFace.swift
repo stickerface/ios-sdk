f68e129f5518781b201fad7e0ab21e830aefa683
Xaker69 <max.xaker41@mail.ru>
2022-03-15 16:25:46 +0300

    + | StikerFace | add snapKit, add onboarding files, add assets file

---
+++ Sources/Common/Helpers/StikerFace.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+public class StikerFace {
+
+    public static let shared = StikerFace()
+
+    public init() {
+        
+    }
+    
+    
+
+}
---
+++ Sources/Module/Onboarding/View/OnboardingView.swift
@@ -0,0 +1,20 @@
+//
+//  OnboardingView.swift
+//  StikerFace
+//
+//  Created by Maxim Khrabryi on 15.03.2022.
+//
+
+import UIKit
+
+class OnboardingView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
