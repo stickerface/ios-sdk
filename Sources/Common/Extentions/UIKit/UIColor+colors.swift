c972d1f601c8f2646a298d9a55eed6be6185a158
Xaker69 <max.xaker41@mail.ru>
2022-03-15 17:47:52 +0300

    + | UIColor | add custom colors

---
+++ Sources/Common/Extentions/UIKit/UIColor+colors.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+extension UIColor {
+    
+    open class var accentBrand: UIColor {
+        return UIColor(hex: 0x45AEF5)
+    }
+    
+    open class var defaultWhite: UIColor {
+        return UIColor(hex: 0xFFFFFF)
+    }
+    
+}
