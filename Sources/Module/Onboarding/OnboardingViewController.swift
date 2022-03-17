d8f169e7e7ba5124b32ec9408719ab84bd8a3e43
Xaker69 <max.xaker41@mail.ru>
2022-03-18 02:19:59 +0300

    + | StikerFace | add lost content

--- Sources/Module/Onboarding/OnboardingViewController.swift
+++ Sources/Module/Onboarding/OnboardingViewController.swift
@@ -5,7 +5,11 @@ class OnboardingViewController: ViewController<OnboardingView> {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
+        mainView.continueButton.addTarget(self, action: #selector(continueButtonTapped), for: .touchUpInside)
+    }
+    
+    @objc private func continueButtonTapped() {
+        navigationController?.pushViewController(ConnectWalletViewController(), animated: false)
     }
     
 }
