4d22689f8bc2ede319e9747f52a3b76afe872ac9
Xaker69 <max.xaker41@mail.ru>
2022-03-25 07:56:34 +0300

    + | Main | add main page file

---
+++ Sources/Module/StickerFace/MainPage/StikerFaceMainViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class StikerFaceMainViewController: ViewController<StikerFaceMainView> {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+}
