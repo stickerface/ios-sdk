7ffb508e3414114a70b79066843769e960c7ec9c
Maksim Khrabryi <Max.xaker41@mail.ru>
2022-04-28 10:26:25 +0300

    * | Bundle | change resource bundle name

--- Sources/Common/Extentions/Foundation/Bundle+Resource.swift
+++ Sources/Common/Extentions/Foundation/Bundle+Resource.swift
@@ -4,13 +4,15 @@ extension Bundle {
     
     public static let resourceBundle: Bundle = {
         let myBundle = Bundle(for: StickerFaceFonts.self)
-
+        
         guard let resourceBundleURL = myBundle.url(
-            forResource: "StickerFaceSDK", withExtension: "bundle")
-            else { fatalError("StickerFaceSDK.bundle not found!") }
+            forResource: "StickerFace", withExtension: "bundle")
+            else { fatalError("StickerFace.bundle not found!") }
 
+        print("===", resourceBundleURL)
+        
         guard let resourceBundle = Bundle(url: resourceBundleURL)
-            else { fatalError("Cannot access StickerFaceSDK.bundle!") }
+            else { fatalError("Cannot access StickerFace.bundle!") }
 
         return resourceBundle
     }()
