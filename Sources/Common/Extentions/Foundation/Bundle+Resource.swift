4a0fdcdd51b9073798a78d1eabd3ee44aafb7044
Maksim Khrabryi <Max.xaker41@mail.ru>
2022-04-29 05:33:54 +0300

    - | Sticker Face | remove comment

--- Sources/Common/Extentions/Foundation/Bundle+Resource.swift
+++ Sources/Common/Extentions/Foundation/Bundle+Resource.swift
@@ -8,8 +8,6 @@ extension Bundle {
         guard let resourceBundleURL = myBundle.url(
             forResource: "StickerFace", withExtension: "bundle")
             else { fatalError("StickerFace.bundle not found!") }
-
-        print("===", resourceBundleURL)
         
         guard let resourceBundle = Bundle(url: resourceBundleURL)
             else { fatalError("Cannot access StickerFace.bundle!") }
