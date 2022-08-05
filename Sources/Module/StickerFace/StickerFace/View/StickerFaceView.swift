902b93a490fa3eece9f54f14bbaff6489372e5b2
Xaker69 <max.xaker41@mail.ru>
2022-08-05 08:48:09 +0300

    Update StickerFaceView.swift

--- Sources/Module/StickerFace/StickerFace/View/StickerFaceView.swift
+++ Sources/Module/StickerFace/StickerFace/View/StickerFaceView.swift
@@ -106,9 +106,9 @@ class StickerFaceView: RootView {
         }
         
         avatarView.snp.makeConstraints { make in
-            make.top.equalTo(safeAreaLayoutGuide.snp.top).offset(85.0)
+            make.top.equalTo(safeAreaLayoutGuide.snp.top).offset(59.0)
             make.centerX.equalToSuperview()
-            make.size.equalTo(AvatarView.Layout.avatarImageViewHeight)
+            make.size.equalTo(232.0)
         }
                 
         backgroundImageView.snp.makeConstraints { make in
