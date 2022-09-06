336688cda13b54becccadaa0580affad14b6273d
Maksim Khrabryi <Max.xaker41@mail.ru>
2022-09-06 18:29:48 +0300

    update bottom offset in layers collection

--- Sources/Module/StickerFace/Editor/View/StickerFaceEditorPageView.swift
+++ Sources/Module/StickerFace/Editor/View/StickerFaceEditorPageView.swift
@@ -6,7 +6,7 @@ class StickerFaceEditorPageView: RootView {
     let collectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         let view = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        view.contentInset = UIEdgeInsets(top: 0.0, left: 16.0, bottom: 116.0, right: 16.0)
+        view.contentInset = UIEdgeInsets(top: 0.0, left: 16.0, bottom: 64.0, right: 16.0)
         view.alwaysBounceVertical = true
         view.showsVerticalScrollIndicator = false
         view.backgroundColor = .clear
