9f463be8011be22dd6bf759aa40f405c2aff987c
zloysergunya <zloysergunya@mail.ru>
2022-10-06 21:34:30 +0700

    * | Extentions | Update colors

--- Sources/Common/Extentions/UIKit/UIColor+colors.swift
+++ Sources/Common/Extentions/UIKit/UIColor+colors.swift
@@ -2,31 +2,31 @@ import UIKit
 
 extension UIColor {
     
-    open class var sfAccentBrand: UIColor {
+    public class var sfAccentBrand: UIColor {
         return UIColor(hex: 0x45AEF5)
     }
     
-    open class var sfAccentSecondary: UIColor {
+    public class var sfAccentSecondary: UIColor {
         return UIColor(hex: 0x151C29)
     }
     
-    open class var sfDefaultWhite: UIColor {
+    public class var sfDefaultWhite: UIColor {
         return UIColor(hex: 0xFFFFFF)
     }
     
-    open class var sfTextPrimary: UIColor {
+    public class var sfTextPrimary: UIColor {
         return UIColor(hex: 0x151C29)
     }
     
-    open class var sfTextSecondary: UIColor {
+    public class var sfTextSecondary: UIColor {
         return UIColor(hex: 0x9FA5AE)
     }
     
-    open class var sfSeparatorLight: UIColor {
+    public class var sfSeparatorLight: UIColor {
         return UIColor(hex: 0xE5E5EA)
     }
     
-    open class var sfDisabled: UIColor {
+    public class var sfDisabled: UIColor {
         return UIColor(hex: 0xE0E6EB)
     }
     
